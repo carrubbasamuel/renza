66cea62998d094711506fa1c1f559c6346073841
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-04-29 20:37:00 +0200

    fix

--- script.js
+++ script.js
@@ -8,8 +8,4 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
